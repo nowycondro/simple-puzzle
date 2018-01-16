e12f5dcb0b83b82d853b9b316047a37781441b59
Nowy Condro <nowy_cheung@yahoo.com>
2018-01-16 21:34:34 +0100

    Simplify it more

--- js/index.js
+++ js/index.js
@@ -4,48 +4,39 @@
   const set = (object, attr, value) => {
     object[attr] = value
   }
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   const one = document.querySelector.bind(document)
   const all = document.querySelectorAll.bind(document)
   const addBackground = (elem, index) => set(elem.style, 'backgroundImage', `url(./img/${images[index]})`)
   const each = (items, func) => items.forEach(func)
   const map = (items, func) => items.map(func)
   const run = (...funcs) => map(funcs, ([func, ...args]) => func.apply(null, args))
-  const click = (elem, func) => elem.addEventListener('click', func)
+  const click = (elem, func) => elem.addEventListener('click', func) || elem  // Guess why "||" is here
   const toggle = elem => elem.currentTarget.classList.toggle('flip')
-  const flipImage = elem => elem.classList.add('flip')
+  const flip = elem => elem.classList.add('flip')
   const append = (from, to) => from + to
   const generateCard = num => `<div class="card"><div class="front back">${num}</div></div>`
   const shuffle = array => array.sort(() => Math.random() - 0.5)
   const [containerNode, resetButton] = run([one, '.card-container'], [one, 'button.reset'])
-
-  // Cache the images
-  each(images, filename => set(new window.Image(), 'src', `./img/${filename}`))
-
-  // Generate cards
-  set(containerNode, 'innerHTML', [1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((prev, curr) => append(prev, generateCard(curr)), ''))
-
-  const [cards, backs] = run([all, '.card'], [all, '.back'])
-
-  // Random number in each card for animation
-  const setRandomNumber = () => each(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]), (num, index) => set(backs[index], 'innerHTML', num))
-
-  // Add toggle function to each card
-  each(cards, card => click(card, toggle))
-
-  let ramdomTimer
+  const [,, [cards, backs]] = run(
+    [each, images, filename => set(new window.Image(), 'src', `./img/${filename}`)],  // Cache the images
+    [set, containerNode, 'innerHTML', numbers.reduce((prev, curr) => append(prev, generateCard(curr)), '')],  // Generate all cards
+    [run, [all, '.card'], [all, '.back']] // Get the cards
+  )
+  let animationTimer
   const reset = () => {
-    ramdomTimer = run(
-      [clearInterval, ramdomTimer],
+    animationTimer = run(
+      [clearInterval, animationTimer],
       [shuffle, images],
-      [each, cards, flipImage],
-      [setInterval, setRandomNumber, 200],
-      [setTimeout, () => run([each, cards, addBackground]), 500],
-      [setTimeout, () => run([clearInterval, ramdomTimer]), 1000]
+      [each, cards, flip],  // Flip the card
+      [setInterval, () => each(shuffle(numbers), (num, index) => set(backs[index], 'innerHTML', num)), 200],  // Random number animation
+      [setTimeout, () => each(cards, addBackground), 500], // Add delay before change the background
+      [setTimeout, () => clearInterval(animationTimer), 1000] // Clear the random number animation
     )[3]
   }
 
-  // Reset button
-  click(resetButton, reset)
-
-  reset()
+  run(
+    [each, cards, card => click(card, toggle)], // Add toggle function to each card
+    [click, resetButton, reset] // Reset button
+  )[1].click()
 })()
