6ded01e8dd4833d34f4354ff7944dfbc85cb3937
Nowy Condro <nowy_cheung@yahoo.com>
2018-01-16 20:32:52 +0100

    Use template string

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 (() => {
-  const images = ['DS085528.jpg', 'DS140041.jpg', 'DS144620.jpg', 'DS170218.jpg', 'DSC00396.JPG', 'DSC00560.JPG', 'DSC00628.JPG', 'DSC00710.JPG', 'DSC01885.JPG', 'DSC03715.JPG', 'DSC03726.JPG', 'DSC03736.JPG', 'DSC03773.JPG', 'DSC03875.JPG', 'DSC03896.JPG']
+  const images = ['DS085528.jpg', 'DS140041.jpg', 'DS144620.jpg', 'DS170218.jpg', 'DSC00396.JPG', 'DSC00560.JPG', 'DSC00628.JPG',
+    'DSC00710.JPG', 'DSC01885.JPG', 'DSC03715.JPG', 'DSC03726.JPG', 'DSC03736.JPG', 'DSC03773.JPG', 'DSC03875.JPG', 'DSC03896.JPG']
   const set = (object, attr, value) => {
     object[attr] = value
   }
   const one = document.querySelector.bind(document)
   const all = document.querySelectorAll.bind(document)
-  const addBackground = (elem, index) => set(elem.style, 'backgroundImage', 'url("./img/' + images[index] + '")')
+  const addBackground = (elem, index) => set(elem.style, 'backgroundImage', `url(./img/${images[index]})`)
   const each = (items, func) => items.forEach(func)
   const map = (items, func) => items.map(func)
   const run = (...funcs) => map(funcs, ([func, ...args]) => func.apply(null, args))
@@ -18,7 +19,7 @@
   const [containerNode, resetButton] = run([one, '.card-container'], [one, 'button.reset'])
 
   // Cache the images
-  each(images, filename => set(new window.Image(), 'src', './img/' + filename))
+  each(images, filename => set(new window.Image(), 'src', `./img/${filename}`))
 
   // Generate cards
   set(containerNode, 'innerHTML', [1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((prev, curr) => append(prev, generateCard(curr)), ''))
